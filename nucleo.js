cdd3e95bb872944ad851df9d5839761536ff0635
julio1994sitece <monopolio400@gmail.com>
2021-08-05 22:57:58 -0300

    Arquivado

---
+++ nucleo.js
@@ -0,0 +1,9 @@
+document.getElementById("cadastro").style.display="none";
+document.getElementById("subcadastro").style.display="block";
+document.getElementById("animacaocentral").style.display="block";
+
+
+function app(){
+         document.getElementById("cadastro").style.display="block";
+         document.getElementById("animacaocentral").style.display="none";
+         }
